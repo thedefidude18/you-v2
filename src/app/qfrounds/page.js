b01cf6c1fb27c17bf34d18c28e8de36172407228
vksr8102 <116914527+vksr8102@users.noreply.github.com>
2024-08-21 15:38:39 +0530

    qfcard

--- src/app/qfrounds/page.js
+++ src/app/qfrounds/page.js
@@ -2,6 +2,7 @@
 "use client";
 import Banner from "@/components/Banner/Banner";
 import Card from "@/components/qfPageComponent/Card";
+import QfCard from "@/components/qfPageComponent/QfCard";
 import { getQFRound } from "@/utils";
 import React, { useEffect, useState } from "react";
 import { useAccount } from "wagmi";
@@ -21,7 +22,7 @@ function page() {
     if (chainId) initQFRound()
   }, [chainId])
   return (
-    <div>
+    <div className="p-4">
       <Banner
         text={qfRound ? qfRound.leftTime > 0 ? `${qfRound.qfRound.title} has started, it will end in ${Math.floor(qfRound.leftTime / (24 * 60 * 60))} days.` : `${qfRound.qfRound.title} has ended` : ""}
         image={qfRound ? qfRound.qfRound.imgUrl : "/svgs/proj/BannerProduct.svg"}
@@ -31,6 +32,7 @@ function page() {
       {/* <div className=" grid sm:grid-cols-3 grid-cols-1 w-full gap-2 p-2"> */}
 
       <Card qfRound={qfRound} />
+      <QfCard/>
       {/* </div> */}
     </div>
   );
